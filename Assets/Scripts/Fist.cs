58c411355123417e8820c6a25b8039ae378c267d
chrispirina <33334781+chrispirina@users.noreply.github.com>
2018-08-02 01:37:13 +1000

    movement and punch stuff

---
+++ Assets/Scripts/Fist.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fist : MonoBehaviour
+{
+    public GameObject leftFist;
+    public GameObject rightFist;
+    public float range = 10;
+    public bool isLPunching = false;
+    public bool isRPunching = false;
+    public Vector3 lStart;
+    public Vector3 rStart;
+    public float lhitTime = 0;
+    public float rhitTime = 0;
+
+
+    private void Awake()
+    {
+        lStart = leftFist.transform.localPosition;
+        rStart = rightFist.transform.localPosition;
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer();
+
+        if (Input.GetKeyDown(KeyCode.Mouse0) && lhitTime <= 0)
+            isLPunching = true;
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+            isRPunching = true;
+
+        if (isLPunching == true)
+        {
+            lhitTime = 0.1f;
+            leftFist.transform.Translate(0, 0, range);
+            isLPunching = false;
+        }
+
+        if (isLPunching == false && lhitTime <= 0f)
+        {
+            leftFist.transform.localPosition = lStart;
+        }
+
+
+        if (isRPunching == true)
+        {
+            rhitTime = 0.1f;
+            rightFist.transform.Translate(0, 0, range);
+            isRPunching = false;
+        }
+
+        if (isRPunching == false && rhitTime <= 0f)
+        {
+            rightFist.transform.localPosition = rStart;
+        }
+    }
+
+    private void timer()
+    {
+        if (lhitTime > 0)
+        {
+            lhitTime -= Time.deltaTime;
+        }
+        if (rhitTime > 0)
+        {
+            rhitTime -= Time.deltaTime;
+        }
+    }
+
+}
+
+
+
+
+
+
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 0.3f;
+    public Vector3 mousePos;
+    public Transform gameObjectToRotate;
+    public float charge = 0;
+    public float chargelimit = 4;
+    public float chargespeed = 0.5f;
+    public Vector3 startPos;
+    public Vector3 endPos;
+    public float distcovered;
+    public float fracJourney;
+
+    Vector3 middleOfScreen;
+
+    // Use this for initialization
+    void Start()
+    {
+        middleOfScreen = new Vector3(Screen.width / 2, Screen.height / 2, 0f);
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        /*transform.LookAt(Input.mousePosition.x);
+        Cursor.visible = true;
+        {
+            mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y - transform.position.y, Camera.main.transform.position.z));
+            transform.LookAt(mousePos);
+        }*/
+         
+
+
+
+        Vector3 camVec = Input.mousePosition - middleOfScreen;
+        Vector3 flipped = new Vector3(camVec.x, 0.0f, camVec.y);
+        gameObjectToRotate.LookAt(flipped);
+    
+
+
+
+
+
+        //MOVEMENT
+        if (Input.GetKey(KeyCode.W))
+            transform.Translate(0, 0, speed);
+        if (Input.GetKey(KeyCode.S))
+            transform.Translate(0, 0, -speed);
+        if (Input.GetKey(KeyCode.D))
+            transform.Translate(speed, 0, 0);
+        if (Input.GetKey(KeyCode.A))
+            transform.Translate(-speed, 0, 0);
+
+
+
+        if (Input.GetKey(KeyCode.Space) && charge < chargelimit)
+        {
+            charge += Time.deltaTime * chargespeed;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            startPos = transform.position;
+            float zStartPosMod = startPos.z += charge;
+            Vector3 endPos = new Vector3(startPos.x, startPos.y, zStartPosMod);
+            transform.position = Vector3.Lerp(startPos, endPos, fracJourney);
+            charge = 0f;
+            print(endPos);
+            print(startPos.x);  
+        }
+
+
+    }
+    
+    
+
+
+}
